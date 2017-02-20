87d5633f898b392c23f36769a2314b0f67de45e9
JoeHowarth <josephehowarth@gmail.com>
2017-02-20 17:12:18 -0500

    2/20

--- cs231n/classifiers/linear_svm.py
+++ cs231n/classifiers/linear_svm.py
@@ -32,26 +32,31 @@ def svm_loss_naive(W, X, y, reg):
         for j in xrange(num_classes):
             if j == y[i]:
                 continue
-            margin = scores[j] - correct_class_score + 1 # note delta = 1
+            margin = scores[j] - correct_class_score + 1 # note delta = 1 
             if margin > 0:
-                loss += margin
                 count += 1
-                # Right now the loss is a sum over all training examples, but we want it
-                # to be an average instead so we divide by num_train.
-                loss /= num_train
+                dW[:, j] += X[i]
+                loss += margin
+        # for correct row
+        dW[:, y[i]] += -count * X[i]
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= num_train
+    dW   /= num_train
+    dW   += reg * W
 
-                # Add regularization to the loss.
-                loss += 0.5 * reg * np.sum(W * W)
+    # Add regularization to the loss.
+    loss += 0.5 * reg * np.sum(W * W)
 
-                #############################################################################
-                # TODO:                                                                     #
-                # Compute the gradient of the loss function and store it dW.                #
-                # Rather that first computing the loss and then computing the derivative,   #
-                # it may be simpler to compute the derivative at the same time that the     #
-                # loss is being computed. As a result you may need to modify some of the    #
-                # code above to compute the gradient.                                       #
-                #############################################################################
-        dW[i] = -count * X[i] 
+    #############################################################################
+    # TODO:                                                                     #
+    # Compute the gradient of the loss function and store it dW.                #
+    # Rather that first computing the loss and then computing the derivative,   #
+    # it may be simpler to compute the derivative at the same time that the     #
+    # loss is being computed. As a result you may need to modify some of the    #
+    # code above to compute the gradient.                                       #
+    #############################################################################
+        
     return loss, dW
 
 
@@ -69,7 +74,9 @@ def svm_loss_vectorized(W, X, y, reg):
     # Implement a vectorized version of the structured SVM loss, storing the    #
     # result in loss.                                                           #
     #############################################################################
-    pass
+    
+    score = X.dot(W)
+    
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
